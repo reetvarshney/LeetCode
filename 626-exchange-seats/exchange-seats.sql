e7a888362d968d7ada666a533c22fc33867aa555
Reet Varshney <159458511+reetvarshney@users.noreply.github.com>
2026-08-30 03:26:18 +0530

    Time: 346 ms (84.06%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+SELECT 
+CASE 
+WHEN ID%2 = 1 AND ID +1 <= (SELECT MAX(ID) FROM SEAT)
+THEN ID +1
+
+WHEN ID%2 =0
+THEN ID -1
+
+ELSE
+ID
+END
+
+AS ID, STUDENT 
+FROM SEAT
+ORDER BY ID
